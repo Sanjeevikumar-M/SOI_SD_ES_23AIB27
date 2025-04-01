415ae0849552fd92cca38eaad34b8377eaf238d8
Sanjeevikumar-M <sanjeevikumar2225@gmail.com>
2025-04-01 11:32:12 +0530

    Updated code

--- one.js
+++ one.js
@@ -1,9 +1,9 @@
 removeDup([1,2,2,2,10]);
 function removeDup(arr) {
     let result = [];
-    for (let value of arr) {
-        if (!result.includes(value)) {
-            result.push(value);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            result.push(arr[i]);
         }
     }
     return result;
